a3374d42edb807f8f43a92cae150eaf2649e30e2
CesarRengifo <crengi94@gmail.com>
2018-12-13 20:17:44 -0500

    testing jueves

--- app/models/subscriptor.rb
+++ app/models/subscriptor.rb
@@ -1,4 +1,9 @@
 class Subscriptor < ApplicationRecord
    
     validates :name, :email, presence: true   # Esto es una validacion de presencia
+  
+  def capitalize_name
+    self.name = self.name.capitalize
+  end
+
 end
